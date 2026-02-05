7233e205c1ea8b163d4ebc150229335b47c3d00a
sachdevalucky04-maker <sachdevalucky04@gmail.com>
2026-02-05 09:38:23 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+c={"hi" : "hey",
+    "hello" : "helloooo",
+    "how are you" : "i am good"
+}
+r=(input("what do you want to say :"))
+if r == "hi":
+ print(c["hi"])
+elif r== "hello":
+ print(c["hello"])
+elif r== "how are you":
+ print (c["how are you"])
